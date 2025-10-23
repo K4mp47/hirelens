58e6881f5a2d2b94fcc88dfde8c339b08645a3d6
K4mp47 <campagnoloalberto5@gmail.com>
2025-10-23 17:34:44 +0200

    fix: adding new upload page

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadPage = () => {
+  return (
+    <main className="flex items-center justify-center min-h-screen w-full">
+      <h1 className="text-4xl font-bold">Upload Your Resume</h1>
+    </main>
+  )
+}
+
+export default UploadPage
